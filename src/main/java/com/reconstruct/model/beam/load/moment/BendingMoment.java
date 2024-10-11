416bc36e8b76e8814e29b3910d272a5bffee4e5c
Dawid Pilch <dawidpilch001@gmail.com>
2024-10-11 10:18:00 +0200

    bending moment

---
+++ src/main/java/com/reconstruct/model/beam/load/moment/BendingMoment.java
@@ -0,0 +1,30 @@
+package com.reconstruct.model.beam.load.moment;
+
+import com.reconstruct.model.value.Magnitude;
+import com.reconstruct.model.value.Position;
+
+public class BendingMoment {
+    private final Position position;
+    private final Magnitude magnitude;
+
+    public static BendingMoment clockwise(Position position, Magnitude magnitude) {
+        return new BendingMoment(position, magnitude.negative());
+    }
+
+    public static BendingMoment counterClockwise(Position position, Magnitude magnitude) {
+        return new BendingMoment(position, magnitude.positive());
+    }
+
+    private BendingMoment(Position position, Magnitude magnitude) {
+        this.position = position;
+        this.magnitude = magnitude;
+    }
+
+    public Position position() {
+        return position;
+    }
+
+    public Magnitude magnitude() {
+        return magnitude;
+    }
+}
