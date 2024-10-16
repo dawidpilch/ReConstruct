1ba9c6575a345ef250575e4636b0c79c77f9e9ce
Dawid Pilch <dawidpilch001@gmail.com>
2024-10-16 15:10:13 +0200

    Span record

--- src/main/java/com/reconstruct/model/beam/span/Span.java
+++ src/main/java/com/reconstruct/model/beam/span/Span.java
@@ -1,5 +1,13 @@
 package com.reconstruct.model.beam.span;
 
-public interface Span {
+import com.reconstruct.model.beam.section.Section;
+import com.reconstruct.model.value.Length;
 
+import java.util.Objects;
+
+public record Span(Length length, Section section) {
+    public Span {
+        Objects.requireNonNull(length);
+        Objects.requireNonNull(section);
+    }
 }
