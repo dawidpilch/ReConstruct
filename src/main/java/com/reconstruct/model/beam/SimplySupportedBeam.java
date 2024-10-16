4d44879de88dc40869a694ee8bca5cacb169cc66
Dawid Pilch <dawidpilch001@gmail.com>
2024-10-16 15:10:24 +0200

    Update SimplySupportedBeam.java

--- src/main/java/com/reconstruct/model/beam/SimplySupportedBeam.java
+++ src/main/java/com/reconstruct/model/beam/SimplySupportedBeam.java
@@ -6,8 +6,7 @@ import com.reconstruct.model.beam.loading.Loading;
 import com.reconstruct.model.beam.loading.moment.BendingMoment;
 import com.reconstruct.model.beam.loading.point.HorizontalPointLoad;
 import com.reconstruct.model.beam.loading.point.VerticalPointLoad;
-import com.reconstruct.model.beam.section.Section;
-import com.reconstruct.model.value.Length;
+import com.reconstruct.model.beam.span.Span;
 import com.reconstruct.model.value.Magnitude;
 import com.reconstruct.model.beam.value.Position;
 
@@ -15,26 +14,48 @@ import java.util.List;
 import java.util.Map;
 
 public class SimplySupportedBeam implements Beam {
-    public static SimplySupportedBeam withRollerOnStart(Length length, Section section) {
-        return new SimplySupportedBeam(length, section, Position.of(length.doubleValue()), Position.of(0));
+
+    /**
+     *
+     * @param span {@link Span} of the beam
+     * @return {@link SimplySupportedBeam} with roller support on start
+     */
+    public static SimplySupportedBeam withRollerSupportOnStart(Span span) {
+        return new SimplySupportedBeam(span, Position.of(span.length().doubleValue()), Position.of(0));
     }
 
-    public static SimplySupportedBeam withRollerOnEnd(Length length, Section section) {
-        return new SimplySupportedBeam(length, section, Position.of(0), Position.of(length.doubleValue()));
+    /**
+     *
+     * @param span {@link Span} of the beam
+     * @return {@link SimplySupportedBeam} with roller support on end
+     */
+    public static SimplySupportedBeam withRollerSupportOnEnd(Span span) {
+        return new SimplySupportedBeam(span, Position.of(0), Position.of(span.length().doubleValue()));
     }
 
-    protected static SimplySupportedBeam custom(Length length, Section section, Position pinnedPosition, Position rollerPosition) {
-        return new SimplySupportedBeam(length, section, pinnedPosition, rollerPosition);
+    /**
+     *
+     * @param span {@link Span} of the beam
+     * @param pinnedPosition {@link Position} of pinned support
+     * @param rollerPosition {@link Position} of roller support
+     * @throws IllegalArgumentException if {@link Position} of either support is out of range of the {@link Span},
+     * or if {@link Position}s of both supports are equal
+     * @return {@link SimplySupportedBeam} with custom support positions
+     */
+    public static SimplySupportedBeam withCustomSupportPositions(Span span, Position pinnedPosition, Position rollerPosition) {
+        if (!pinnedPosition.inRangeOf(span) || !rollerPosition.inRangeOf(span))
+            throw new IllegalArgumentException("Support's position must be defined in range of the span's length.");
+        if (pinnedPosition.equals(rollerPosition))
+            throw new IllegalArgumentException("Supports must be defined in different positions.");
+        return new SimplySupportedBeam(span, pinnedPosition, rollerPosition);
     }
 
-    private final Length length;
-    private final Section section;
+    private final Span span;
     private final Position pinnedPosition;
     private final Position rollerPosition;
 
-    private SimplySupportedBeam(Length length, Section section, Position pinnedPosition, Position rollerPosition) {
-        this.length = length;
-        this.section = section;
+    private SimplySupportedBeam(Span span, Position pinnedPosition, Position rollerPosition) {
+        this.span = span;
         this.pinnedPosition = pinnedPosition;
         this.rollerPosition = rollerPosition;
     }
