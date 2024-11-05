4ea7f03264a4e3ae7539e3fa53b9e7727b06c0ec
Dawid Pilch <dawidpilch001@gmail.com>
2024-11-05 22:25:35 +0100

    RCBeamAnalysisDesign fxml and controller

--- src/main/java/com/reconstruct/view/RCBeamAnalysisDesignController.java
+++ src/main/java/com/reconstruct/view/RCBeamAnalysisDesignController.java
@@ -1,18 +1,54 @@
 package com.reconstruct.view;
 
+import com.reconstruct.model.beam.SimplySupportedBeam;
+import com.reconstruct.model.beam.loading.Loading;
+import com.reconstruct.model.beam.loading.point.VerticalPointLoad;
+import com.reconstruct.model.beam.section.Rectangular;
+import com.reconstruct.model.beam.span.Span;
+import com.reconstruct.model.beam.value.Position;
+import com.reconstruct.model.value.Length;
+import com.reconstruct.model.value.Magnitude;
+import com.reconstruct.model.value.PositiveDouble;
 import javafx.fxml.FXML;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.XYChart;
+
+import java.util.List;
+import java.util.Map;
 
 public class RCBeamAnalysisDesignController {
     @FXML public AreaChart<Double, Double> areaChart;
 
     @FXML public void initialize() {
+        SimplySupportedBeam simplySupportedBeam = SimplySupportedBeam.withRollerSupportOnEnd(
+                new Span(
+                        Length.of(10),
+                        new Rectangular(
+                                PositiveDouble.of(10),
+                                PositiveDouble.of(10)
+                        )
+                )
+        );
+
+        Loading loading = new Loading(
+                List.of(
+                        VerticalPointLoad.directedDownwards(Position.of(2d), Magnitude.of(-4)),
+                        VerticalPointLoad.directedUpwards(Position.of(5d), Magnitude.of(8))
+                ),
+                List.of(),
+                List.of()
+        );
+
+        Map<Position, Magnitude> positionMagnitudeMap = simplySupportedBeam.bendingMomentDiagram(loading);
         XYChart.Series<Double, Double> series = new XYChart.Series<>();
         series.setName("SheerForceDemo");
-        series.getData().add(new XYChart.Data<>(1d,1d));
-        series.getData().add(new XYChart.Data<>(1d,7d));
-        series.getData().add(new XYChart.Data<>(6d,6d));
+        for (Map.Entry<Position, Magnitude> entry : positionMagnitudeMap.entrySet()) {
+            series.getData().add(new XYChart.Data<>(entry.getKey().doubleValue(), entry.getValue().doubleValue()));
+        }
+
+//        series.getData().add(new XYChart.Data<>(1d,1d));
+//        series.getData().add(new XYChart.Data<>(1d,7d));
+//        series.getData().add(new XYChart.Data<>(6d,6d));
         areaChart.getData().add(series);
         areaChart.autosize();
     }
