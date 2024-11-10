dc613f03a08a41c6aaf74c77255b49f5bb3f6117
Dawid Pilch <dawidpilch001@gmail.com>
2024-11-10 17:08:29 +0100

    Update RCBeamAnalysisDesignController.java

--- src/main/java/com/reconstruct/view/RCBeamAnalysisDesignController.java
+++ src/main/java/com/reconstruct/view/RCBeamAnalysisDesignController.java
@@ -1,6 +1,8 @@
 package com.reconstruct.view;
 
+import com.reconstruct.model.beam.BendingMomentDiagram;
 import com.reconstruct.model.beam.LoadingAnalysis;
+import com.reconstruct.model.beam.SheerForceDiagram;
 import com.reconstruct.model.beam.SimplySupportedBeam;
 import com.reconstruct.model.beam.loading.Loading;
 import com.reconstruct.model.beam.loading.point.VerticalPointLoad;
@@ -15,46 +17,71 @@ import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import org.apache.commons.math3.util.MathUtils;
+
 import java.util.List;
 import java.util.Map;
 
 public class RCBeamAnalysisDesignController {
     @FXML public AreaChart<Double, Double> areaChart;
+    @FXML public NumberAxis xAxis;
+    @FXML public NumberAxis yAxis;
 
     @FXML public void initialize() {
+        double length = 14;
         SimplySupportedBeam simplySupportedBeam = SimplySupportedBeam.withCustomSupportPositions(
                 new Span(
-                        Length.of(10),
+                        Length.of(length),
                         new Rectangular(
                                 PositiveDouble.of(10),
                                 PositiveDouble.of(10)
                         )
-                ), Position.of(2), Position.of(7)
+                ), Position.of(3), Position.of(14)
         );
 
         Loading loading = new Loading(
                 List.of(
-                        VerticalPointLoad.directedDownwards(Position.of(1), Magnitude.of(-6)),
-                        VerticalPointLoad.directedUpwards(Position.of(5), Magnitude.of(5))
+                        VerticalPointLoad.directedDownwards(Position.of(3), Magnitude.of(-8)),
+                        VerticalPointLoad.directedUpwards(Position.of(7), Magnitude.of(5.64))
                 ),
                 List.of(),
                 List.of()
         );
 
         LoadingAnalysis loadingAnalysis = simplySupportedBeam.loadingAnalysis(loading);
+        BendingMomentDiagram bendingMomentDiagram = loadingAnalysis.bendingMomentDiagram();
+        SheerForceDiagram sheerForceDiagram = loadingAnalysis.sheerForceDiagram();
+
+        xAxis.setAutoRanging(false);
+        xAxis.setTickUnit(length / 20);
+        xAxis.setUpperBound(length);
+        xAxis.setLowerBound(0d);
+
+        var maxBendingMoment = bendingMomentDiagram.stream().map(Map.Entry::getValue).map(Magnitude::doubleValue).map(Math::abs).max(Double::compareTo).orElse(0d);
+        var maxSheerForce = sheerForceDiagram.stream().map(Map.Entry::getValue).map(Magnitude::doubleValue).map(Math::abs).max(Double::compareTo).orElse(0d);
+        double maxMagnitude = Double.max(maxBendingMoment, maxSheerForce);
+
+        yAxis.setAutoRanging(false);
+        yAxis.setTickUnit(maxMagnitude / 10);
+        yAxis.setUpperBound(maxMagnitude);
+        yAxis.setLowerBound(-maxMagnitude);
+
         XYChart.Series<Double, Double> bendingMomentSeries = new XYChart.Series<>();
         XYChart.Series<Double, Double> sheerForceSeries = new XYChart.Series<>();
         bendingMomentSeries.setName("Bending Moment");
         sheerForceSeries.setName("Sheer Force");
-        for (Map.Entry<Position, Magnitude> entry : loadingAnalysis.bendingMomentDiagram()) {
+        for (Map.Entry<Position, Magnitude> entry : bendingMomentDiagram) {
             bendingMomentSeries.getData().add(new XYChart.Data<>(entry.getKey().doubleValue(), entry.getValue().doubleValue()));
         }
-        for (Map.Entry<Position, Magnitude> entry : loadingAnalysis.sheerForceDiagram()) {
+        for (Map.Entry<Position, Magnitude> entry : sheerForceDiagram) {
             sheerForceSeries.getData().add(new XYChart.Data<>(entry.getKey().doubleValue(), entry.getValue().doubleValue()));
         }
 
         areaChart.getData().add(bendingMomentSeries);
         areaChart.getData().add(sheerForceSeries);
-        areaChart.autosize();
     }
 }
