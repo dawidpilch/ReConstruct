9a85e6e77ff8c2d0a9f1a86c30e58a27edb7d9b8
Dawid Pilch <dawidpilch001@gmail.com>
2025-02-05 16:50:35 +0100

    Update ReConstructApp.java

--- src/main/java/com/reconstruct/ReConstructApp.java
+++ src/main/java/com/reconstruct/ReConstructApp.java
@@ -4,22 +4,21 @@ import com.reconstruct.view.component.RCWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class ReConstructApp extends Application {
-
+    public final static Image APP_ICON = new Image((Objects.requireNonNull(ReConstructApp.class.getResourceAsStream("icon.png"))));
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ReConstructApp.class.getResource("fxml/new-calculation-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ReConstructApp.class.getResource("fxml/beam-analysis-design-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.minHeightProperty().setValue(400);
-        stage.minWidthProperty().setValue(400);
-        stage.setTitle("ReConstruct");
-        stage.setScene(scene);
-        new RCWindow(stage).show();
+        new RCWindow(scene, "ReConstruct", Modality.NONE).show();
     }
 
     public static void main(String[] args) {
