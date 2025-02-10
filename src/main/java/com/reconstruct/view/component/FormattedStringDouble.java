9f4bd6fe9b8bd4bc7a2b8f64c583bd6abc02f915
Dawid Pilch <dawidpilch001@gmail.com>
2025-02-10 16:20:13 +0100

    Update FormattedStringDouble.java

--- src/main/java/com/reconstruct/view/component/FormattedStringDouble.java
+++ src/main/java/com/reconstruct/view/component/FormattedStringDouble.java
@@ -3,7 +3,7 @@ package com.reconstruct.view.component;
 public record FormattedStringDouble(double value) {
     @Override
     public String toString() {
-        String formatted = String.format("%.3f", value);
-        return formatted.endsWith(".000") ? formatted.split("\\.000")[0] : formatted;
+        String formatted = String.format("%.2f", value);
+        return formatted.endsWith(".00") ? formatted.split("\\.00")[0] : formatted;
     }
 }
