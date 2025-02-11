60295abd5c350e0e81698252e1a029d9c108d07f
dawidpilch <dawidpilch001@gmail.com>
2025-02-11 16:21:53 +0100

    Update RectangularSectionViewModel.java

--- src/main/java/com/reconstruct/view/viewmodel/RectangularSectionViewModel.java
+++ src/main/java/com/reconstruct/view/viewmodel/RectangularSectionViewModel.java
@@ -1,6 +1,6 @@
 package com.reconstruct.view.viewmodel;
 
 public class RectangularSectionViewModel {
-    public final AppendableProperty<Double> depthProperty = new PositiveDoubleAppendableProperty(450d, "Depth (mm)");
-    public final AppendableProperty<Double> widthProperty = new PositiveDoubleAppendableProperty(250d, "Width (mm)");
+    public final AppendableProperty<Double> depthProperty = new PositiveDoubleAppendableProperty(450d, "Depth [mm]");
+    public final AppendableProperty<Double> widthProperty = new PositiveDoubleAppendableProperty(250d, "Width [mm]");
 }
